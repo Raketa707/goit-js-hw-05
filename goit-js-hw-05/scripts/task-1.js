db6f44031c1d6723fba6b30b62b5ea94672e351b
Raketa707 <60396808+Raketa707@users.noreply.github.com>
2020-06-21 21:44:48 +0300

    Add files via upload

--- goit-js-hw-05/scripts/task-1.js
+++ goit-js-hw-05/scripts/task-1.js
@@ -1,18 +1,23 @@
 "use strict"
 
-const Account = function(login, email) {
+
+const Account = function Account(login,email) {
+    
     this.login = login;
     this.email = email;
-}
+    
 
-Account.prototype.getInfo = function() {
+}
+Account.prototype.getInfo = function(){
     console.log(`login: ${this.login}, email: ${this.email}`);
 }
 
-console.log(Account.prototype.getInfo); // function
+ console.log(Account.prototype.getInfo);
 
-const mango = new Account("Mangozedog", "mango@dog.woof");
-mango.getInfo(); // Login: Mangozedog, Email: mango@dog.woof
-
-const poly = new Account("Poly", "poly@mail.com");
-poly.getInfo(); // Login: Poly, Email: poly@mail.com
+ const mango = new Account('Mangozedog','mango@dog.woof');
+ mango.getInfo();
+ 
+ const poly = new Account('Poly','poly@mail.com');
+ poly.getInfo();
+ console.log(poly)
+ console.log(mango)
